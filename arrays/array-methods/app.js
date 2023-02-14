3079eb64e9e723a306e6a7f426c559c66ebdca15
David Yakin <78755939+David-Yakin@users.noreply.github.com>
2023-02-14 17:23:08 +0200

    array methods

---
+++ arrays/array-methods/app.js
@@ -0,0 +1,85 @@
+/********** array methods **********/
+/***** length *****/
+// const array = ["one", "two", "three"];
+
+// console.log(array.length);
+// console.log(array);
+
+/***** Sort *****/
+// const array = [3, 1, 2];
+// const array = ["a", "c", "b"];
+
+// const sortedArray = array.sort();
+
+// console.log(sortedArray);
+
+/***** reverse *****/
+
+// const array = ["a", "c", "b"];
+
+// const newArray = [...array];
+// const sortedArray = newArray.sort();
+// const reverseArray = sortedArray.reverse();
+
+// console.log(array);
+// console.log(reverseArray);
+
+// const sortArray = array => {
+//   if (typeof array !== "object") return "Please enter an array!";
+//   if (!array.length) return array;
+//   const newArray = [...array];
+//   const sortedArray = newArray.sort();
+//   return sortedArray;
+// };
+
+// const sortAndReversedArray = array => {
+//   if (typeof array !== "object") return "Please enter an array!";
+//   if (!array.length) return array;
+//   const newArray = [...array];
+//   const sortedArray = newArray.sort();
+//   const reverseArray = sortedArray.reverse();
+//   return reverseArray;
+// };
+
+// const array = ["a", "c", "b"];
+
+// const arrayFixed = sortArray(array);
+// const arrayFixed = sortArray([]);
+
+// console.log(arrayFixed);
+
+// const arrayFixed = sortAndReversedArray(array);
+// console.log(arrayFixed);
+
+/***** push *****/
+// const array = ["a", "c", "b"];
+// array.push("d");
+
+// console.log(array);
+
+/***** unshift *****/
+// const array = ["a", "c", "b"];
+// array.unshift("d");
+
+// console.log(array);
+
+/***** pop *****/
+// const array = ["a", "b", "c"];
+// const newArray = [...array];
+// const popItem = newArray.pop();
+
+// newArray.unshift(popItem);
+
+// console.log(array);
+// console.log(newArray);
+// console.log(popItem);
+
+/***** shift *****/
+// const array = ["a", "b", "c"];
+// const newArray = [...array];
+// const shiftItem = newArray.shift();
+// newArray.push(shiftItem);
+
+// console.log(array);
+// console.log(shiftItem);
+// console.log(newArray);
